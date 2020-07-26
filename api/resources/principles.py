bbb9b3fad493c71d5f031eab6b3fbdfbd3992dd3
Xednom <monde.lacanlalay@gmail.com>
2020-07-26 18:53:33 +0800

    added perform create and update for created_by and updated_by

--- api/resources/principles.py
+++ api/resources/principles.py
@@ -9,9 +9,18 @@ class AgilePrincipleSerializer(serializers.ModelSerializer):
     class Meta:
         model = AgilePrinciple
         fields = "__all__"
+        read_only_fields = ["created_by", "updated_by"]
 
 
 class AgilePrincipleViewSet(viewsets.ModelViewSet):
     queryset = AgilePrinciple.objects.all()
     serializer_class = AgilePrincipleSerializer
     permission_classes = [IsAdminUserOrReadOnly]
+
+    def perform_create(self, serializer):
+        current_user = self.request.user.username
+        return serializer.save(created_by=current_user)
+
+    def perform_update(self, serializer):
+        current_user = self.request.user.username
+        return serializer.save(updated_by=current_user)
--- api/resources/values.py
+++ api/resources/values.py
@@ -9,9 +9,18 @@ class AgileValuesSerializer(serializers.ModelSerializer):
     class Meta:
         model = AgileValue
         fields = "__all__"
+        read_only_fields = ["created_by", "updated_by"]
 
 
 class AgileValueViewSet(viewsets.ModelViewSet):
     queryset = AgileValue.objects.all()
     serializer_class = AgileValuesSerializer
     permission_classes = [IsAdminUserOrReadOnly]
+
+    def perform_create(self, serializer):
+        current_user = self.request.user.username
+        return serializer.save(created_by=current_user)
+    
+    def perform_update(self, serializer):
+        current_user = self.request.user.username
+        return serializer.save(updated_by=current_user)
