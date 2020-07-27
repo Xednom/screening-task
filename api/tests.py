c00e567b4cc504156c5089a4c3c72b4ec0a96144
Xednom <monde.lacanlalay@gmail.com>
2020-07-28 01:09:10 +0800

    added tests, and applied black to urls and tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,34 @@
+from django.urls import reverse
 from django.test import TestCase
+from api.models import AgileValue, AgilePrinciple
 
-# Create your tests here.
+
+class ApiTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        AgileValue.objects.create(title="testtitle_value", body="testbody_value")
+        AgilePrinciple.objects.create(
+            title="testtitle_principle", body="testbody_principle"
+        )
+
+    def test_value_max_length(self):
+        value = AgileValue.objects.get(id=1)
+
+        value_max_length = value._meta.get_field("title").max_length
+        self.assertEquals(value_max_length, 300)
+
+    def test_principle_max_length(self):
+        principle = AgilePrinciple.objects.get(id=1)
+
+        principle_max_length = principle._meta.get_field("title").max_length
+        self.assertEquals(principle_max_length, 300)
+
+    def test_api_status_code(self):
+        response_values = self.client.get(
+            "http://127.0.0.1:8000/api/v1/agile-values/", follow=True
+        )
+        response_principles = self.client.get(
+            "http://127.0.0.1:8000/api/v1/agile-principles/", follow=True
+        )
+        self.assertEquals(response_values.status_code, 200)
+        self.assertEquals(response_principles.status_code, 200)
--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,7 @@ from api.resources.values import AgileValueViewSet
 
 router = DefaultRouter()
 
-router.register("agile-values", AgileValueViewSet)
-router.register("agile-principles", AgilePrincipleViewSet)
+router.register("agile-values", AgileValueViewSet, basename="agile-list")
+router.register("agile-principles", AgilePrincipleViewSet, basename="agile-principles")
 
 urlpatterns = [path("", include(router.urls))]
